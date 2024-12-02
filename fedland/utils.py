36be3978bee9cc12e19061d05df52b50b904610f
legendkxtx <legendkxtx@gmail.com>
2024-12-02 19:55:18 +0100

    Added plot function based on Plotly

--- fedland/utils.py
+++ fedland/utils.py
@@ -5,6 +5,9 @@ from typing import Tuple, Optional
 from fedland.database_models.experiment import Experiment
 import matplotlib.pyplot as plt
 import seaborn as sns
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 
 def read_experiment_data(path: str) -> Tuple[pd.DataFrame, pd.DataFrame, list]:
@@ -139,3 +142,109 @@ def plot_results_overview(df, results_path: str = "results", errorbars: Optional
 
     plt.tight_layout()
     plt.show()
+
+def plot_results_overview_plotly(df, n_clients, result_path = './results/', metrics = ['Path-norm', 'Training Accuracy', 'Training Loss', 'Testing Accuracy', 'Testing Loss']):
+    """
+    Create consistent visualization of federated learning experiments.
+    Based on Plotly
+    """
+
+    # Define some useful colours
+    colours = px.colors.qualitative.Dark24[:2] + px.colors.qualitative.Dark24[6:8] + px.colors.qualitative.Dark24[14:] + [px.colors.qualitative.Dark24[5]]
+    
+    experiment_ids = df["experiment_id"].unique()
+    n_experiments = len(experiment_ids)
+    n_clients_experiments = df[['experiment_id', 'client_index']].groupby('experiment_id').nunique()
+    n_valid_experiments = (n_clients_experiments == n_clients).sum().iloc[0].item()
+    
+    row_count = 1
+    
+    fig = make_subplots(rows = n_valid_experiments, cols = len(metrics),
+                        row_titles = [get_experiment_description(i) for i in experiment_ids],
+                        subplot_titles = metrics * n_experiments,
+                        horizontal_spacing = 0.05,
+                        vertical_spacing = 0.05)
+    
+    for j in range(n_experiments):
+        df_experiment = df[df['experiment_id'] == experiment_ids[j]]
+        n_clients_experiment = len(df_experiment['client_index'].unique())
+        
+        if n_clients != n_clients_experiment:
+            print(f"WARN: Experiment id '{experiment_ids[j]}' does not have the same number of clients.\nThe results are not plotted")
+            continue
+        
+        # Local Path-norm
+        [[fig.add_trace(go.Scatter(x = df_experiment[df_experiment['client_index'] == i].index + 1,
+                                   y = df_experiment[df_experiment['client_index'] == i]['path_norm'],
+                                   mode = 'lines',
+                                   legendgroup = 'Client ' + str(i),
+                                   line_color = colours[i],
+                                   name = 'Client ' + str(i),
+                                   showlegend = True if j == 0 else False), row = row_count, col = 1),
+          # Update x- and y-axes properties
+          fig.update_xaxes(title_text = 'Testing Iteration', row = row_count, col = 1),
+          fig.update_yaxes(title_text = 'Path-norm', row = row_count, col = 1)] for i in df_experiment['client_index'].unique()]
+        # Global Path-norm
+        fig.add_trace(go.Scatter(x = df_experiment[df_experiment['client_index'] == 0].index + 1,
+                                 y = df_experiment[df_experiment['client_index'] == 0]['global_path_norm'],
+                                 mode = 'lines',
+                                 legendgroup = 'Global Path-norm',
+                                 line = dict(dash = 'dot'),
+                                 line_color = colours[n_clients],
+                                 name = 'Global Path-norm',
+                                 showlegend = True if j == 0 else False), row = row_count, col = 1)
+        # Training Accuracy
+        [[fig.add_trace(go.Scatter(x = df_experiment[df_experiment['client_index'] == i].index + 1,
+                                   y = df_experiment[df_experiment['client_index'] == i]['train_accuracy'],
+                                   mode = 'lines',
+                                   legendgroup = 'Client ' + str(i),
+                                   line_color = colours[i],
+                                   name = 'Client ' + str(i),
+                                   showlegend = False), row = row_count, col = 2),
+          # Update x- and y-axes properties
+          fig.update_xaxes(title_text = 'Testing Iteration', row = row_count, col = 2),
+          fig.update_yaxes(title_text = 'Training Accuracy', row = row_count, col = 2)] for i in df_experiment['client_index'].unique()]
+        # Training Loss
+        [[fig.add_trace(go.Scatter(x = df_experiment[df_experiment['client_index'] == i].index + 1,
+                                   y = df_experiment[df_experiment['client_index'] == i]['train_loss'],
+                                   mode = 'lines',
+                                   legendgroup = 'Client ' + str(i),
+                                   line_color = colours[i],
+                                   name = 'Client ' + str(i),
+                                   showlegend = False), row = row_count, col = 3),
+          # Update x- and y-axes properties
+          fig.update_xaxes(title_text = 'Testing Iteration', row = row_count, col = 3),
+          fig.update_yaxes(title_text = 'Training Loss', row = row_count, col = 3)] for i in df_experiment['client_index'].unique()]
+        # Testing Accuracy
+        [[fig.add_trace(go.Scatter(x = df_experiment[df_experiment['client_index'] == i].index + 1,
+                                   y = df_experiment[df_experiment['client_index'] == i]['test_accuracy'],
+                                   mode = 'lines',
+                                   legendgroup = 'Client ' + str(i),
+                                   line_color = colours[i],
+                                   name = 'Client ' + str(i),
+                                   showlegend = False), row = row_count, col = 4),
+          # Update x- and y-axes properties
+          fig.update_xaxes(title_text = 'Testing Iteration', row = row_count, col = 4),
+          fig.update_yaxes(title_text = 'Testing Accuracy', row = row_count, col = 4)] for i in df_experiment['client_index'].unique()]
+        # Testing Loss
+        [[fig.add_trace(go.Scatter(x = df_experiment[df_experiment['client_index'] == i].index + 1,
+                                   y = df_experiment[df_experiment['client_index'] == i]['test_loss'],
+                                   mode = 'lines',
+                                   legendgroup = 'Client ' + str(i),
+                                   line_color = colours[i],
+                                   name = 'Client ' + str(i),
+                                   showlegend = False), row = row_count, col = 5),
+          # Update x- and y-axes properties
+          fig.update_xaxes(title_text = 'Testing Iteration', row = row_count, col = 5),
+          fig.update_yaxes(title_text = 'Testing Loss', row = row_count, col = 5)] for i in df_experiment['client_index'].unique()]
+    
+        row_count += 1
+    
+    fig.update_layout(height = 900 * n_valid_experiments,
+                      width = 900 * len(metrics),
+                      legend = dict(orientation = 'h'),
+                      title_text = str(n_clients) + '-client Experiment Results')
+    fig.update_xaxes(range = [df.index.min() - 5, df.index.max() + 5])
+    
+    fig.show()
+    fig.write_image(result_path + '/overivew.png', height = 900 * n_valid_experiments, width = 900 * len(metrics))
